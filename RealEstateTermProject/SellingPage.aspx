0cfa2580f42340cf5e7ead83a34c023f90e7781c
jn653 <juannavarro653@gmail.com>
2023-04-09 13:01:43 -0700

    I added a selling page and created a new nav for the home seller account so that i can changed the tabs for the nav bar

--- RealEstateTermProject/SellingPage.aspx
+++ RealEstateTermProject/SellingPage.aspx
@@ -1,10 +1,10 @@
-﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="LandingPageforHomeSeller.aspx.cs" Inherits="RealEstateTermProject.LandingPageforHomeSeller" %>
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="SellingPage.aspx.cs" Inherits="RealEstateTermProject.SellingPage" %>
 
 <!DOCTYPE html>
 
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
-      <script src="js/navHomeSeller.js"></script>
+   <script src="js/navHomeSeller.js"></script>
     <link href="styles/LandingPage.css" rel="stylesheet" />
     <link href="Content/bootstrap.min.css" rel="stylesheet" />
     <script src="Scripts/bootstrap.bundle.min.js"></script>
@@ -12,9 +12,8 @@
     <title></title>
 </head>
 <body>
-    <script>navHomeSeller()</script>
     <form id="form1" runat="server">
-       
+        <script>navHomeSeller()</script>
         <content>
             <welcometext>Welcome! Find your dream home with us!</welcometext>
             <img src="https://thumbor.forbes.com/thumbor/fit-in/900x510/https://www.forbes.com/home-improvement/wp-content/uploads/2022/07/download-23.jpg" />
---
+++ RealEstateTermProject/js/navHomeSeller.js
@@ -0,0 +1,68 @@
+﻿/*
+ ||============================== HOW TO USE ==============================||
+    
+     *TO INSERT INTO SOLUTION:
+    - Drag and drop nav.css from the "styles" folder into the solution
+    - Drag and drop nav.js from the "js" folder into the solution
+    - Insert "<script> nav() </script>" at the very top of the body
+        - The navbar will then create itself, requiring no other actions
+          from the user
+
+    *TO ADD A LINK:
+    - Simply add a new link with the required parameters to the "links"
+      array
+        - The function will automatically add any new links and will update
+          for every page that is using the nav.js function
+
+ ||========================================================================|| 
+ */
+
+
+function navHomeSeller() {
+
+    var head = document.head;
+    var style = document.createElement("link");
+
+    style.type = "text/css";
+    style.rel = "stylesheet";
+    style.href = "styles/nav.css";
+
+    head.appendChild(style); // <----------- Adds the nav styling for ease of use
+
+    body = document.getElementsByTagName("body")[0];
+
+    navBar = document.createElement("nav");
+    navTitle = document.createElement("navTitle");
+    linkBox = document.createElement("linkBox");
+
+    navBar.style
+
+    navTitle.innerHTML = "Willow";
+
+    let links = [
+        {
+            "href": "LandingPageforHomeSeller.aspx",
+            "innerHTML": "Home"
+        },
+        {
+            "href": "SellingPage.aspx",
+            "innerHTML": "Sell"
+        },
+        {
+            "href": "LoginPage.aspx",
+            "innerHTML": "Sign Out"
+        }
+    ];
+
+    for (i = 0; i < links.length; i++) {
+        newlink = document.createElement("a");
+        newlink.href = links[i].href;
+        newlink.innerHTML = links[i].innerHTML;
+        linkBox.appendChild(newlink);
+    }
+
+    navBar.appendChild(navTitle);
+    navBar.appendChild(linkBox);
+
+    body.appendChild(navBar);
+}
