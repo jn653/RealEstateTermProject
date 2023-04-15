a1c0c25b1da4f154016c646d3201a40bdcff7b73
jn653 <juannavarro653@gmail.com>
2023-04-15 08:04:58 -0700

    I made the repeater for showing agents

---
+++ RealEstateTermProject/FindARealEstateAgentPage.aspx
@@ -0,0 +1,32 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="FindARealEstateAgentPage.aspx.cs" Inherits="RealEstateTermProject.FindARealEstateAgentPage" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <script src="js/navHomeSeller.js"></script>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <script>navHomeSeller()</script>
+        <div>
+        </div>
+        <table style="z-index: 1; left: 189px; top: 84px; position: absolute; height: 104px; width: 433px; right: 824px">
+            <tr style="color:darkorchid">
+                <th> Real Estate Agent</th>
+            </tr>
+             <asp:Repeater ID="RealEstateAgentReapeater" runat="server">
+                 <ItemTemplate>
+                <tr>
+                    <td>
+                        <asp:Label ID="lblAgentUsername" runat="server"
+                            Text='<%# DataBinder.Eval(Container.DataItem, "RealEstateAgentUsername") %>'></asp:Label>
+                    </td>
+                </tr>
+                     </ItemTemplate>
+        </asp:Repeater>
+        </table>
+    </form>
+</body>
+</html>
--- RealEstateTermProject/LoginPage.aspx.cs
+++ RealEstateTermProject/LoginPage.aspx.cs
@@ -99,7 +99,7 @@ namespace RealEstateTermProject
             //code for sending the two factor email - have to fix it to get it to work 
 
             //string to = email; //To address    
-            //string from = "fromaddress@gmail.com"; //From address    
+            //string from = "juann1065@gmail.com"; //From address    
             //MailMessage message = new MailMessage(from, to);
 
             //string mailbody = "In this article you will learn how to send a email using Asp.Net & C#";
@@ -109,10 +109,11 @@ namespace RealEstateTermProject
             //message.IsBodyHtml = true;
             //SmtpClient client = new SmtpClient("smtp.gmail.com", 587); //Gmail smtp    
             //System.Net.NetworkCredential basicCredential1 = new
-            //System.Net.NetworkCredential(email, profilePassword);
+            //System.Net.NetworkCredential("juann1065@gmail.com", "password");
             //client.EnableSsl = true;
             //client.UseDefaultCredentials = false;
             //client.Credentials = basicCredential1;
+
             //try
             //{
             //    client.Send(message);
@@ -124,47 +125,50 @@ namespace RealEstateTermProject
             //}
 
 
-            // this code works for validation 
-            if (string.IsNullOrEmpty(txtUsername.Text) || (string.IsNullOrEmpty(txtPassword.Text)) || txtUsername.Text == "Username" && txtPassword.Text == "Password")
-
-            {
-                lblUsername.Text = "You must enter your username";
-                lblUsername.ForeColor = System.Drawing.Color.Red;
-
-                lblPassword.Text = "Your must ener your password";
-                lblPassword.ForeColor = System.Drawing.Color.Red;
-            }
-            else
 
 
-                if (!profileUsername.Equals(txtUsername.Text) && !profilePassword.Equals(txtPassword.Text))
-            {
-                lblUsername.Text = "Invalid Username/Password";
-                lblUsername.ForeColor = System.Drawing.Color.Red;
 
-                lblPassword.Text = "Invalid Username/Password";
-                lblPassword.ForeColor = System.Drawing.Color.Red;
-            }
-            else
+            //// this code works for validation 
+            //if (string.IsNullOrEmpty(txtUsername.Text) || (string.IsNullOrEmpty(txtPassword.Text)) || txtUsername.Text == "Username" && txtPassword.Text == "Password")
 
-            if (profileUsername.Equals(txtUsername.Text) && profilePassword.Equals(txtPassword.Text) && accountType.Equals("Home Seller") && txtUsername.Text.Length != 0 || txtPassword.Text.Length != 0)
-            {
-                Response.Redirect("LandingPageforHomeSeller.aspx");
-            }
-            else
+            //{
+            //    lblUsername.Text = "You must enter your username";
+            //    lblUsername.ForeColor = System.Drawing.Color.Red;
 
-            if (profileUsername.Equals(txtUsername.Text) && profilePassword.Equals(txtPassword.Text) && accountType.Equals("Home Buyer") && txtUsername.Text.Length != 0 || txtPassword.Text.Length != 0)
-            {
+            //    lblPassword.Text = "Your must ener your password";
+            //    lblPassword.ForeColor = System.Drawing.Color.Red;
+            //}
+            //else
 
-                Response.Redirect("LandingPage.aspx");
-            }
-            else
 
-            if (profileUsername.Equals(txtUsername.Text) && profilePassword.Equals(txtPassword.Text) && accountType.Equals("Real Estate Agent") && txtUsername.Text.Length != 0 || txtPassword.Text.Length != 0)
-            {
+            //    if (!profileUsername.Equals(txtUsername.Text) && !profilePassword.Equals(txtPassword.Text))
+            //{
+            //    lblUsername.Text = "Invalid Username/Password";
+            //    lblUsername.ForeColor = System.Drawing.Color.Red;
 
-                Response.Redirect("LandingPageforRealEstateAgent.aspx");
-            }
+            //    lblPassword.Text = "Invalid Username/Password";
+            //    lblPassword.ForeColor = System.Drawing.Color.Red;
+            //}
+            //else
+
+            //if (profileUsername.Equals(txtUsername.Text) && profilePassword.Equals(txtPassword.Text) && accountType.Equals("Home Seller") && txtUsername.Text.Length != 0 || txtPassword.Text.Length != 0)
+            //{
+            //    Response.Redirect("LandingPageforHomeSeller.aspx");
+            //}
+            //else
+
+            //if (profileUsername.Equals(txtUsername.Text) && profilePassword.Equals(txtPassword.Text) && accountType.Equals("Home Buyer") && txtUsername.Text.Length != 0 || txtPassword.Text.Length != 0)
+            //{
+
+            //    Response.Redirect("LandingPage.aspx");
+            //}
+            //else
+
+            //if (profileUsername.Equals(txtUsername.Text) && profilePassword.Equals(txtPassword.Text) && accountType.Equals("Real Estate Agent") && txtUsername.Text.Length != 0 || txtPassword.Text.Length != 0)
+            //{
+
+            //    Response.Redirect("LandingPageforRealEstateAgent.aspx");
+            //}
 
 
 
