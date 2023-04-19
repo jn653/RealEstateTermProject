5f6bd04893af388eb2d9b6a033f159769c1cc46a
jn653 <juannavarro653@gmail.com>
2023-04-19 15:06:24 -0700

    I added some functions to soap

--- RealEstateTermProject/SignupPage.aspx.cs
+++ RealEstateTermProject/SignupPage.aspx.cs
@@ -12,6 +12,7 @@ namespace RealEstateTermProject
 {
     public partial class SignupPage : System.Web.UI.Page
     {
+        SoapUserFunc SoapUser = new SoapUserFunc();
         protected void Page_Load(object sender, EventArgs e)
         {
 
@@ -20,102 +21,66 @@ namespace RealEstateTermProject
         protected void btnSignUp_Click(object sender, EventArgs e)
         {
             // checking to see if string username is already taken in the datbase 
-            SqlCommand objCommand20 = new SqlCommand();
-            objCommand20.CommandType = CommandType.StoredProcedure;
-            objCommand20.CommandText = "TP_validateCreateNewUser";
+            string error =  SoapUser.ValidateSignUp(txtCreateUsername.Text);
 
-
-
-
-            SqlParameter outputParameter30 = new SqlParameter("@accountUsername", SqlDbType.VarChar, 600);
-            outputParameter30.Direction = ParameterDirection.Output;
-            objCommand20.Parameters.Add(outputParameter30);
-
-
-            DBConnect objDB45 = new DBConnect();
-            objDB45.GetDataSet(objCommand20);
-
-            string starterUsername = objCommand20.Parameters["@accountUsername"].Value.ToString();
-
-//---------------------------------------------------------------------------------------------------------------------------------
-
-            // getting the account type
-
-            string AccountType;
-            if (radiobtnHomeSeller.Checked == true)
+            if (string.IsNullOrEmpty(error))
             {
-                AccountType = "Home Seller";
-               
-            }
-            else if (radiobtnRealEstateAgent.Checked == true)
-            {
-                AccountType = "Real Estate Agent";
- 
-            }
+                lblUsername.Text = "Username is good";
+                //---------------------------------------------------------------------------------------------------------------------------------
+
+                // getting the account type
+
+                string AccountType;
+                if (radiobtnHomeSeller.Checked == true)
+                {
+                    AccountType = "Home Seller";
+
+                }
+                else if (radiobtnRealEstateAgent.Checked == true)
+                {
+                    AccountType = "Real Estate Agent";
+
+                }
+                else
+
+                    AccountType = "Home Buyer";
+
+                //adding user account to database
+                SoapUser.addUserAccount(txtCreateUsername.Text, txtCreatePassword.Text, txtEmail.Text, AccountType.ToString(), lblseucrityquestion1question0.Text,
+    lblseucrityquestion2question0.Text, lblseucrityquestion3question.Text, txtSecurityquestion1Answer.Text, txtSecurityquestion2Answer.Text, txtSecurityquestion3Answer.Text);
+
+                //storing username to use across multiple pages
+                Session["Username"] = txtCreateUsername.Text;
+
+                // depedning on what the account type is  will determine what landing page loads
+                if (AccountType.ToString() == "Home Seller")
+                {
+                    //storing username to use across multiple pages
+
+                    Session["Username"] = txtCreateUsername.Text;
+                    Response.Redirect("LandingPageforHomeSeller.aspx");
+                }
+                else if (AccountType.ToString() == "Real Estate Agent")
+                {
+                    //storing username to use across multiple pages
+
+                    Session["Username"] = txtCreateUsername.Text;
+                    Response.Redirect("AddRealEstateInfoPage.aspx");
+                }
+                else
+                    //storing username to use across multiple pages
+
+                    Session["Username"] = txtCreateUsername.Text;
+                Response.Redirect("LandingPage.aspx");
+            
+
+
+        }
             else
+                lblUsername.Text = "Username is taken, please choose a different one";
+            lblUsername.ForeColor = System.Drawing.Color.Red;
+          
 
-                AccountType = "Home Buyer";
-
-            //--------------------------------------------------------------------------------------------------------------------------
-
-            // this code is to add the newly crearted user to the database table UserAccount.
-            //it is commented out just so we dont have to create a new user everytime we test the sign up page
-
-            if (!starterUsername.Equals(txtCreateUsername.Text))
-            {
-
-                SqlCommand objCommand = new SqlCommand();
-                objCommand.CommandType = CommandType.StoredProcedure;
-                objCommand.CommandText = "TP_AddUserAccount";
-
-
-                objCommand.Parameters.AddWithValue("@theUsername", txtCreateUsername.Text);
-                objCommand.Parameters.AddWithValue("@thePassword", txtCreatePassword.Text);
-                objCommand.Parameters.AddWithValue("@accountType", AccountType.ToString());
-                objCommand.Parameters.AddWithValue("@theEmail", txtEmail.Text);
-                objCommand.Parameters.AddWithValue("@FirstSecurityQuestion", lblseucrityquestion1question0.Text);
-                objCommand.Parameters.AddWithValue("@SecondSecurityQuestion", lblseucrityquestion2question0.Text);
-                objCommand.Parameters.AddWithValue("@ThirdSecurityQuestion", lblseucrityquestion3question.Text);
-                objCommand.Parameters.AddWithValue("@FirstAnswer", txtSecurityquestion1Answer.Text);
-                objCommand.Parameters.AddWithValue("@SecondAnswer", txtSecurityquestion2Answer.Text);
-                objCommand.Parameters.AddWithValue("@ThirdAnswer", txtSecurityquestion3Answer.Text);
-
-
-
-
-
-                DBConnect objDB = new DBConnect();
-                DataSet myDataSet;
-                myDataSet = objDB.GetDataSet(objCommand);
-
-                //storing username to use across multiple pages
-                Session["Username"] = txtCreateUsername.Text;
-
-            }
-            else
-                lblUsername.Text = "Username/Password already taken";
-
-
-            // depedning on what the account type is  will determine what landing page loads
-            if (AccountType.ToString() == "Home Seller")
-            {
-                //storing username to use across multiple pages
-
-                Session["Username"] = txtCreateUsername.Text;
-                Response.Redirect("LandingPageforHomeSeller.aspx");
-            }
-            else if (AccountType.ToString() == "Real Estate Agent")
-            {
-                //storing username to use across multiple pages
-
-                Session["Username"] = txtCreateUsername.Text;
-                Response.Redirect("AddRealEstateInfoPage.aspx");
-            }
-            else
-                //storing username to use across multiple pages
-
-                Session["Username"] = txtCreateUsername.Text;
-            Response.Redirect("LandingPage.aspx");
         }
     }
 }
---
+++ RealEstateTermProject/SoapUserFunc.asmx.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Web.UI.WebControls;
+using Utilities;
+
+namespace RealEstateTermProject
+{
+    /// <summary>
+    /// Summary description for SoapUserFunc
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class SoapUserFunc : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string ValidateSignUp(string usernameTextBox)
+        {
+            string errorMessage;
+            SqlCommand objCommand20 = new SqlCommand();
+            objCommand20.CommandType = CommandType.StoredProcedure;
+            objCommand20.CommandText = "TP_validateCreateNewUser";
+
+            objCommand20.Parameters.AddWithValue("@Username", usernameTextBox);
+
+            SqlParameter outputParameter = new SqlParameter("@accountUsername", SqlDbType.VarChar, 600);
+            outputParameter.Direction = ParameterDirection.Output;
+            objCommand20.Parameters.Add(outputParameter);
+
+            DBConnect objDB = new DBConnect();
+            DataSet myDataSet;
+            myDataSet = objDB.GetDataSet(objCommand20);
+
+
+            string profileUsername = objCommand20.Parameters["@accountUsername"].Value.ToString();
+           
+
+            return profileUsername;
+        } 
+
+        public void addUserAccount(string username, string password, string email, string accountType, string firstquestion, string secondquestion, string thirdquestion, string firstanswer, string secondanswer, string thirdanswer)
+        {
+            SqlCommand objCommand = new SqlCommand();
+            objCommand.CommandType = CommandType.StoredProcedure;
+            objCommand.CommandText = "TP_AddUserAccount";
+
+
+            objCommand.Parameters.AddWithValue("@theUsername", username);
+            objCommand.Parameters.AddWithValue("@thePassword", password);
+            objCommand.Parameters.AddWithValue("@accountType", accountType);
+            objCommand.Parameters.AddWithValue("@theEmail", email);
+            objCommand.Parameters.AddWithValue("@FirstSecurityQuestion", firstquestion);
+            objCommand.Parameters.AddWithValue("@SecondSecurityQuestion", secondquestion);
+            objCommand.Parameters.AddWithValue("@ThirdSecurityQuestion", thirdquestion);
+            objCommand.Parameters.AddWithValue("@FirstAnswer", firstanswer);
+            objCommand.Parameters.AddWithValue("@SecondAnswer", secondanswer);
+            objCommand.Parameters.AddWithValue("@ThirdAnswer", thirdanswer);
+
+
+            DBConnect objDB = new DBConnect();
+            DataSet myDataSet;
+            myDataSet = objDB.GetDataSet(objCommand);
+
+           
+        }
+    }
+}
