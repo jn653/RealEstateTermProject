fa671ec5e36b4074402b510c1f1d8201485b04c6
jn653 <juannavarro653@gmail.com>
2023-04-19 08:11:09 -0700

    I put script manager

--- RealEstateTermProject/SellingPage.aspx.cs
+++ RealEstateTermProject/SellingPage.aspx.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Net;
 using System.Web;
@@ -21,25 +23,54 @@ namespace RealEstateTermProject
 
         protected void Button1_Click(object sender, EventArgs e)
         {
-            House house = new House();
-            house.Address = address.Value;
-            house.PropertyType = propertyType.Value;
-            house.HomeSize = int.Parse(homeSize.Value);
-            house.NumberOfBedrooms = int.Parse(numOfBed.Value);
-            house.Amenities = amenities.Value;
-            house.HouseYear = int.Parse(houseYear.Value);
-            house.Garage = garage.Value;
-            house.Utilities = utilities.Value;
-            house.HomeDescription = homeDescription.Value;
-            house.AskingPrice = int.Parse(askingPrice.Value);
-            house.HouseImages = houseImages.Value;
-            house.State = state.Value;
-            house.NumberOfBathrooms = int.Parse(numOfBath.Value);
-            house.City = city.Value;
-            house.SellerID = -1;
-            house.RealEstateID = -1;
+            //retrieving the username for the stored username in login and sign up page to use in other pages
+            string UserAccountName = (string)Session["Username"];
+            //retrieving the account type from the stored session
+            string AccountType = (string)Session["accountType"];
 
-            houseUtils.putHouse(house);
+            // stored procedure to get the user ID
+            SqlCommand objCommand50 = new SqlCommand();
+            objCommand50.CommandType = CommandType.StoredProcedure;
+            objCommand50.CommandText = "TP_RetrieveUserID";
+
+
+            objCommand50.Parameters.AddWithValue("@theUsername", UserAccountName);
+
+
+            SqlParameter outputParameter30 = new SqlParameter("@theID", SqlDbType.Int, 600);
+            outputParameter30.Direction = ParameterDirection.Output;
+            objCommand50.Parameters.Add(outputParameter30);
+
+
+
+
+            DBConnect objDB46 = new DBConnect();
+            objDB46.GetDataSet(objCommand50);
+
+            int UserID = Convert.ToInt32(objCommand50.Parameters["@theID"].Value.ToString());
+
+            
+
+
+            //House house = new House();
+            //house.Address = address.Value;
+            //house.PropertyType = propertyType.Value;
+            //house.HomeSize = int.Parse(homeSize.Value);
+            //house.NumberOfBedrooms = int.Parse(numOfBed.Value);
+            //house.Amenities = amenities.Value;
+            //house.HouseYear = int.Parse(houseYear.Value);
+            //house.Garage = garage.Value;
+            //house.Utilities = utilities.Value;
+            //house.HomeDescription = homeDescription.Value;
+            //house.AskingPrice = int.Parse(askingPrice.Value);
+            //house.HouseImages = houseImages.Value;
+            //house.State = state.Value;
+            //house.NumberOfBathrooms = int.Parse(numOfBath.Value);
+            //house.City = city.Value;
+            //house.SellerID = -1;
+            //house.RealEstateID = -1;
+
+            //houseUtils.putHouse(house);
             //Response.Redirect("AddingHouseInfotoSellPage.aspx");
         }
     }
